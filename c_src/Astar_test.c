03a24ed751dbe0255a73d5f034057ef4c2c17652
John Zhang <szha215@aucklanduni.ac.nz>
2016-10-08 18:17:17 +1300

    added Astar_test

---
+++ c_src/Astar_test.c
@@ -0,0 +1,48 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Astar.h"
+#include "map.h"
+
+int main(void){
+	int* mapp;
+	int* route;
+	uint8 steps = 0;
+	printf("start\n");
+
+	route = malloc(285 * sizeof(short));
+
+	steps = astar(map, route, 146, 6);
+	//free(a);
+
+	printf("manhattan = %i\n", manhattan(6,4,4,14));
+	printf("========================end\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* [] END OF FILE */
